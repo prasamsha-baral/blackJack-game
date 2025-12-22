79a4f85c60bda6d4864abe465b1ee943b1f92935
prasamsha baral <baralprasamsha@gmail.com>
2025-12-22 13:32:30 +0545

    newcard

--- index.js
+++ index.js
@@ -1,28 +1,53 @@
  console.log("js connected")
  
  let firstCard =11;
- let secondCard =10;
+ let secondCard =3;
  let sum = firstCard + secondCard;
+ let cards = [firstCard , secondCard]
  let hasblackjack =false;
  let isAlive = true;
  let message =""
-
+ let playel = document.getElementById("play-el")
+console.log(playel)
+let sumel = document.querySelector("#sum-el")
+let cardel =document.getElementById("card-el")
 function  startGame(){
+   rendergame()
+}
+
+
+
+function rendergame(){
+
+
 
       if(sum<=20){
-            message="do you want to draw a new card?";
+            message="Do you want to draw a new card?";
 
          }
          else if (sum==21){
-            message="wohoo you've won the blackjacck game.";
+            message="Wohoo you've won the blackjacck game.";
             hasblackjack=true;
          }
          else{
             message="You're out of the game.";
             isAlive=false; 
          }
+         playel.textContent=message
+         sumel.textContent=sum
+         cardel.textContent=firstCard + "," + secondCard
+
          console.log(message);
          console.log(hasblackjack)
+      }
+      
+      function newcard(){
+        let card =9
+        sum += card
+       
+        rendergame()
+         cardel.textContent+= "," + card
+         console.log("draw a new card")
       }
    
 
